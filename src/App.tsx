f269fbc3df60344e5d8daa25bafd0050f259867e
Zzelen <cristian.lop.gonzalez@gmail.com>
2026-03-14 18:30:19 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import Router from './router'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
+import Home from "./pages/Home.tsx";
+import HomeValorant from "./pages/valorant/HomeValorant.tsx";
+import HomeMarathon from "./pages/marathon/HomeMarathon.tsx";
+import Agents from "./pages/valorant/Agents.tsx";
+import Weapons from "./pages/valorant/Weapons.tsx";
+import Maps from "./pages/valorant/Maps.tsx";
+import Roles from "./pages/valorant/Roles.tsx";
 
-function App() {
-    return <Router />
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/valorant" element={<HomeValorant />} />
+                <Route path="/valorant/agents" element={<Agents />} />
+                <Route path="/valorant/weapons" element={<Weapons />} />
+                <Route path="/valorant/maps" element={<Maps />} />
+                <Route path="/valorant/roles" element={<Roles />} />
+                <Route path="/marathon" element={<HomeMarathon />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
 export default App
