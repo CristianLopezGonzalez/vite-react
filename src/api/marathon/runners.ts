1060511886761caa0ed5eefba2bac6fc9e6aaffc
Zzelen <cristian.lop.gonzalez@gmail.com>
2026-03-17 21:31:50 +0100

    add marathone runners

--- src/api/marathon/runners.ts
+++ src/api/marathon/runners.ts
@@ -1,1 +1,16 @@
-﻿
+﻿import api from '../axios.ts'
+
+export interface MarathonRunners {
+    runnerId:number
+    runnerName:string
+    description:string
+    model:string
+    icon:string
+}
+
+const getAllRunners = async ():Promise<MarathonRunners> => {
+    const response = await api.get('/marathon/runners');
+    return response.data.data;
+}
+
+export { getAllRunners };
